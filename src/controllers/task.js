1dd29d720276dc58b6bfdfd4317b3655634be3d9
Leks <leks511@bk.ru>
2020-08-13 16:56:28 +0300

    Перенёс логику таска в контроллер

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -4,10 +4,46 @@ import {render, replace, RenderPosition} from "../utils/render.js";
 
 export default class TaskController {
   constructor(container) {
+    this._container = container;
 
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   render(task) {
-
+    const replaceTaskToEdit = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+  
+    const replaceEditToTask = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+  
+    const onEscKeyDown = (evt) => {
+      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+  
+      if (isEscKey) {
+        replaceEditToTask();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+  
+    const taskComponent = new TaskComponent(task);
+    const taskEditComponent = new TaskEditComponent(task);
+  
+    taskComponent.setEditButtonClickHandler(() => {
+      replaceTaskToEdit();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+  
+    taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      replaceEditToTask();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+  
+    render(taskListElement, taskComponent, RenderPosition.BEFOREEND);
   }
 }
